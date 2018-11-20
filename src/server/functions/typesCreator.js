be7689a4b94dea2f317d3b14397577d6b025b9ea
Anna <anna.brakowska91@gmail.com>
2018-11-19 19:08:35 -0500

    refactored functions

--- src/server/functions/typesCreator.js
+++ src/server/functions/typesCreator.js
@@ -1,49 +1,6 @@
-const pluralize = require("pluralize");
+// const pluralize = require("pluralize");
 
-<<<<<<< HEAD
-=======
-// TESTING DATABASE
-// const allTypes = {
-//   players: {
-//     player_id: "integer",
-//     firstname: "character varying",
-//     lastname: "character varying",
-//     birthdate: "date",
-//     country: "character varying"
-//   },
-//   dogs: {
-//     dog_id: "integer",
-//     firstname: "character varying",
-//     lastname: "character varying",
-//     birthdate: "date"
-//   },
-//   students: {
-//     student_id: "integer",
-//     player_name: "text"
-//   },
-//   cats: {
-//     cat_id: "integer",
-//     firstname: "character varying",
-//     lastname: "character varying",
-//     birthdate: "date"
-//   },
-//   tests: {
-//     subject_id: "integer",
-//     subject_name: "text",
-//     higheststudent_id: "integer"
-//   },
-//   foreignTables: {
-//     tests: "students"
-//   },
-//   primaryKeys: {
-//     players: "player_id",
-//     dogs: "dog_id",
-//     cats: "cat_id",
-//     students: "student_id"
-//   }
-// };
-
->>>>>>> 7095f59a6e915e4edf0e94130e5598b0c0a6faad
+//HELPER FUNCTIONS
 function initialCapitalizer(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
@@ -57,183 +14,126 @@ function valueChecker(str) {
       return "String";
       break;
     case "integer":
-      return "Int";
+      return "Integer";
       break;
     case "date":
       return "String";
       break;
-    case "real":
-      return "Float";
+    case "boolean":
+      return "Boolean";
       break;
     case "character":
       return "String";
       break;
-    case "bool":
-      return "Boolean";
+    case "real":
+      return "String";
       break;
-    case "boolean":
-      return "Boolean";
-      break;
-    case "Bool":
-      return "Boolean";
-      break;
-    case "decimal":
-      return "Float";
-      break;
-    case "DECIMAL":
-      return "Float";
-      break;
-    case "int":
-      return "Int";
-      break;
-    default:
-      return "Float";
+    case "numeric":
+      return "Integer";
       break;
   }
 }
 
-function transformObj(obj) {
-  let output = {};
-  output["type Query"] = {};
-  output["type Mutation"] = {};
-  for (key in obj) {
-    let input = "";
-    let i = 0;
-    if (key != "foreignTables" && key != "primaryKeys")
-      for (field in obj[key]) {
-        let count = Object.keys(obj[key]).length;
-        if (field.slice(field.length - 2) != "id") {
-          input += `${field}:${valueChecker(obj[key][field])}, `;
-        }
-        if (i >= count - 1) {
-          //CREATE
-          output["type Mutation"][
-            `create${pluralize.singular(initialCapitalizer(key))}(${input})`
-          ] = ` ${pluralize.singular(initialCapitalizer(key))}`;
+///////////////////////////////////////////////////////TYPES
 
-          output["type Mutation"][
-            `update${pluralize.singular(initialCapitalizer(key))}(${input})`
-          ] = ` ${[pluralize.singular(initialCapitalizer(key))]}`;
-
-          input = "";
-        }
-
-        i++;
-
-        output["type Mutation"][
-          `delete${pluralize.singular(initialCapitalizer(key))}(id:ID)`
-        ] = ` ` + pluralize.singular(initialCapitalizer(key));
-
-        let singular = pluralize.singular(key);
-        output[`type ` + initialCapitalizer(key)] = obj[key];
-        output["type Query"][pluralize.singular(key)] = initialCapitalizer(
-          singular
-        );
-        output["type Query"][key] = `[${initialCapitalizer(singular)}]`;
-<<<<<<< HEAD
-        output["type Query"][`${key}byID(id:ID)`] = `[${initialCapitalizer(
-          singular
-        )}]`;
-=======
->>>>>>> 7095f59a6e915e4edf0e94130e5598b0c0a6faad
-      }
-  }
-  return output;
-}
-
-function mergeToString(obj) {
+//QUERY TYPE
+function queriesCreator(obj) {
+  let allTables = Object.keys(obj);
   let output = ``;
-  let i = 0;
-  for (key in obj) {
-    output += `\r\n` + key + ` { `;
 
-    for (field in obj[key]) {
-      let counts = Object.keys(obj[key]).length;
-      i++;
-      output += `\r\n   ${field}:${obj[key][field]}`;
-      if (i === counts) {
-        output += "\r\n     }";
-        i = 0;
+  allTables.map(table => {
+    output += `
+        ${table}:[${initialCapitalizer(table)}]
+        ${table}ByID(id:ID):[${initialCapitalizer(table)}]`;
+  });
+  return output;
+}
+
+//ALL TYPES
+function allTypesCreator(obj) {
+  let allTables = Object.keys(obj);
+  //FINAL STRING OUTPUT
+  let output = ``;
+
+  allTables.map(table => {
+    if (table !== "primaryKeys" && table !== "foreignTables") {
+      let allFields = Object.keys(obj[table]);
+      let fieldsMapped = ``;
+
+      allFields.map((field, index) => {
+        fieldsMapped += `
+      ${field}:${valueChecker(obj[table][field])}`;
+        if (obj.foreignTables[table] && index >= allFields.length - 1) {
+          fieldsMapped += `
+      ${table}: ${initialCapitalizer(obj.foreignTables[table])}`;
+        }
+      });
+      output += `type ${initialCapitalizer(table)} {  ${fieldsMapped}
+    }
+
+    `;
+    }
+  });
+  return output;
+}
+
+function mutationCreator(obj) {
+  //ALL TABLE NAMES
+  let allTables = Object.keys(obj);
+  //FINAL STRING OUTPUT
+  let output = ``;
+  //ITERATE OVER ALL TABLES EXCEPT TWO LAST TABLES
+  for (let i = 0; i < allTables.length - 2; i++) {
+    //CURRENT TABLE
+    let table = allTables[i];
+    //GET ALL FIELDS FOR CURRENT TABLE
+    let allFields = Object.keys(obj[allTables[i]]);
+    //CREATE AN INPUT STRING
+    let input = `${allFields[0]}:ID, {`;
+
+    for (let j = 1; j <= allFields.length - 1; j++) {
+      let field = allFields[j];
+
+      let valueOfField = valueChecker(obj[table][field]);
+      input += `${field}: ${valueOfField} `;
+      if (j !== allFields.length - 1) {
+        input += `, `;
+      } else if (j === allFields.length - 1) {
+        input += `}`;
       }
     }
+    output += `
+        create${initialCapitalizer(table)}(${input}): [${initialCapitalizer(
+      table
+    )}]
+        delete${initialCapitalizer(table)}(${
+      allFields[0]
+    }:ID): [${initialCapitalizer(table)}]
+        update${initialCapitalizer(table)}(${input}): [${initialCapitalizer(
+      table
+    )}]`;
   }
-  output += " \r\n }";
-  output = output.replace(/\, \)/g, ")");
-  output = output.replace(/\: /g, ": ");
   return output;
 }
 
-function relations(obj1, obj2) {
-  let output = obj1;
-
-  for (let key in obj2) {
-    let check = "type " + initialCapitalizer(obj2[key]);
-
-    if (obj1.hasOwnProperty(check)) {
-      let type = pluralize.singular(key);
-      obj1[check][key] = `[${initialCapitalizer(type)}]`;
+function typeDefsReturner(str1, str2, str3) {
+  return `
+  const typeDefs = \`{
+    type Query { ${str1}
     }
-  }
-  return output;
-}
 
-function changeValues(obj) {
-  let output = {};
-  for (key in obj) {
-    for (field in obj[key]) {
-      if (
-        obj[key][field] === "character varying" ||
-        obj[key][field] === "text" ||
-        obj[key][field].includes("char")
-      ) {
-        obj[key][field] = "String";
-      } else if (obj[key][field] === "integer") {
-        obj[key][field] = "Int";
-      } else if (obj[key][field] === "date") {
-        obj[key][field] = "String";
-      } else if (obj[key][field].toLowerCase() === "real") {
-        obj[key][field] = "Float";
-      } else if (obj[key][field] === "boolean") {
-        obj[key][field] = "Boolean";
-      } else if (obj[key][field] === "numeric") {
-        obj[key][field] = "Float";
-      } else if (obj[key][field].toLowerCase() === "decimal") {
-        obj[key][field] = "Float";
-      }
+    type Mutation { ${str2}
     }
-  }
-  return obj;
+
+      ${str3}
+  \`
+  
+
+  module.exports = typeDefs;
+  `;
 }
 
-function addQueryType(obj) {
-  let output = obj;
-  obj["Query"] = {};
-  for (let key in obj) {
-    obj["Query"];
-  }
-}
-
-function finalSingulizer(obj) {
-  let output = obj;
-  for (let key in obj) {
-    let splitted = key.split(" ");
-    obj[` type ` + pluralize.singular(splitted[1])] = obj[key];
-    delete obj[key];
-  }
-  output += "}";
-  return output;
-}
-
-const transform = obj => {
-  let typeDefs = transformObj(obj);
-  let final = changeValues(typeDefs);
-  let related = relations(final, obj["foreignTables"]);
-  let singularized = finalSingulizer(related);
-  let queryTypeAdded = addQueryType(singularized);
-  let string = mergeToString(related);
-  return "const typeDefs = ` " + string + "\r\n `;";
-};
-<<<<<<< HEAD
+/////////////////////////////////////////// RESOLVERS
 
 function queryResolver(obj) {
   let output = ``;
@@ -260,129 +160,79 @@ function queryResolver(obj) {
 
   return output;
 }
-=======
-// let transformedTostring = transform(allTypes);
 
-function queryResolver(str, obj) {
-  //PROCESSING QUERY PORTION
-  let splittedTypes = str.split("type");
-  let query = splittedTypes[2];
+function mutationResolver(obj) {
+  // let splitted = str.split("type")[3];
+  // let fields = splitted
+  //   .replace(/Mutation {/g, "")
+  //   .replace(/}/g, "")
+  //   .replace(/  +/g, " ")
+  //   .replace(/\n/g, "")
+  //   .trim();
 
-  let fields = query
-    .replace(/Query {/g, "")
-    .replace(/]/g, "")
-    .replace(/]/g, "")
-    .replace(/\}/g, "")
-    .replace(/\[/g, "")
-    .replace(/\r\n/g, "")
-    .replace(/  +/g, " ")
-    .trim();
-  let splitted = fields.split(" ");
-  let final = splitted.map(el => el.split(":"));
+  // let fieldsSplitted = fields.split("\r");
+  // let output = ``;
 
-  let output = ``;
-  final.map((el, index) => {
-    if (index % 2 === 0) {
-      output += `
-      ${el[0]}(parent, {id}, ctx, info) {
-        client.query("SELECT*FROM ${pluralize(el[0].toString())} where ${
-        obj.primaryKeys[pluralize(el[0].toString())]
-      }= id", 
-        (err,result)=> {
-          if(err) throw new Error("Error querying all ${el[0]}")
-          return result;
-        });
-      },
-      `;
-    } else {
-      output += `
-      ${el[0]}(parent, args, ctx, info) {
-          client.query("SELECT*FROM ${el[0]}", (err,result)=>{
-            if(err) throw new Error("Error querying all ${el[0]}")
-            return result;
-          })
-      
-      },
-      `;
-    }
-  });
-  return `const Query = { ${output} \n };`;
-}
-// console.log(queryResolver(transformedTostring, allTypes));
->>>>>>> 7095f59a6e915e4edf0e94130e5598b0c0a6faad
+  // fieldsSplitted.map((el, index) => {
+  //   let element = el.split("):");
+  //   let pluralized =
+  //     obj.primaryKeys[pluralize(element[1].trim()).toLowerCase()];
+  //   let singularized = obj.primaryKeys[element[1].trim().toLowerCase()];
+  //   let tableName = element[1].trim().toLowerCase();
+  //   let tableNamePl = pluralize(element[1].trim().toLowerCase());
+  //   let from;
+  //   if (obj[tableName]) {
+  //     from = tableName;
+  //   } else {
+  //     from = tableNamePl;
+  //   }
 
-function mutationResolver(str, obj) {
-  let splitted = str.split("type")[3];
-  let fields = splitted
-    .replace(/Mutation {/g, "")
-    .replace(/}/g, "")
-    .replace(/  +/g, " ")
-    .replace(/\n/g, "")
-    .trim();
-
-  let fieldsSplitted = fields.split("\r");
-  let output = ``;
-
-  fieldsSplitted.map((el, index) => {
-    let element = el.split("):");
-    let pluralized =
-      obj.primaryKeys[pluralize(element[1].trim()).toLowerCase()];
-    let singularized = obj.primaryKeys[element[1].trim().toLowerCase()];
-    let tableName = element[1].trim().toLowerCase();
-    let tableNamePl = pluralize(element[1].trim().toLowerCase());
-    let from;
-    if (obj[tableName]) {
-      from = tableName;
-    } else {
-      from = tableNamePl;
-    }
-
-    if (
-      element[0].toString().startsWith(" delete") &&
-      (pluralized || singularized)
-    ) {
-      output += `delete${element[
-        element.length - 1
-      ].trim()}(parent, args, {id}, info) {
-    client.query("DELETE FROM ${from} WHERE ${pluralized ||
-        singularized} = id", (err,result)=>{
-      if(err) throw new Error("Error deleting");
-      return result;
-    })
-    },
-    `;
-    } else if (
-      element[0].toString().startsWith(" create") &&
-      (pluralized || singularized)
-    ) {
-      output += `create${pluralize.singular(
-        element[element.length - 1].trim()
-      )}(parent, args, {id}, info) {
-    client.query("create FROM ${from} WHERE ${pluralized ||
-        singularized} = id", (err,result)=>{
-      if(err) throw new Error("Error creating");
-      return result;
-    })
-    },
-    `;
-    } else if (
-      element[0].toString().startsWith(" upda") &&
-      (pluralized || singularized)
-    ) {
-      output += `update${element[
-        element.length - 1
-      ].trim()}(parent, args, {id}, info) {
-          client.query("update FROM ${from} WHERE ${pluralized ||
-        singularized} = id", (err,result)=>{
-              if(err) throw new Error("Error creating");
-              return result;
-            })
-            },
-            `;
-    }
-  });
-<<<<<<< HEAD
-  return output;
+  //   if (
+  //     element[0].toString().startsWith(" delete") &&
+  //     (pluralized || singularized)
+  //   ) {
+  //     output += `delete${element[
+  //       element.length - 1
+  //     ].trim()}(parent, args, {id}, info) {
+  //   client.query("DELETE FROM ${from} WHERE ${pluralized ||
+  //       singularized} = id", (err,result)=>{
+  //     if(err) throw new Error("Error deleting");
+  //     return result;
+  //   })
+  //   },
+  //   `;
+  //   } else if (
+  //     element[0].toString().startsWith(" create") &&
+  //     (pluralized || singularized)
+  //   ) {
+  //     output += `create${pluralize.singular(
+  //       element[element.length - 1].trim()
+  //     )}(parent, args, {id}, info) {
+  //   client.query("create FROM ${from} WHERE ${pluralized ||
+  //       singularized} = id", (err,result)=>{
+  //     if(err) throw new Error("Error creating");
+  //     return result;
+  //   })
+  //   },
+  //   `;
+  //   } else if (
+  //     element[0].toString().startsWith(" upda") &&
+  //     (pluralized || singularized)
+  //   ) {
+  //     output += `update${element[
+  //       element.length - 1
+  //     ].trim()}(parent, args, {id}, info) {
+  //         client.query("update FROM ${from} WHERE ${pluralized ||
+  //       singularized} = id", (err,result)=>{
+  //             if(err) throw new Error("Error creating");
+  //             return result;
+  //           })
+  //           },
+  //           `;
+  //   }
+  // });
+  // return output;
+  return "Should have some mutations here";
 }
 
 function returnResolvers(str1, str2) {
@@ -390,12 +240,12 @@ function returnResolvers(str1, str2) {
 const psql = require('../psqlAdapter').psql;
 
 const resolvers = {
-      Query:{
+  Query:{
             ${str1}
-        },
+      },
 
-      Mutation:{
-        ${str2}
+  Mutation:{
+            ${str2}
       }
   };
   
@@ -404,15 +254,11 @@ const resolvers = {
 }
 
 module.exports = {
-  transform,
+  allTypesCreator,
+  queriesCreator,
+  mutationCreator,
+  typeDefsReturner,
+  returnResolvers,
   queryResolver,
-  mutationResolver,
-  returnResolvers
+  mutationResolver
 };
-=======
-  return `const Mutation = { \r\n ${output} \r\n};
-  module.exports = Mutation;`;
-}
-
-module.exports = { transform, queryResolver, mutationResolver };
->>>>>>> 7095f59a6e915e4edf0e94130e5598b0c0a6faad
