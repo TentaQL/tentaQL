98663ac2bfa3fd08a518185e343997f33c00af16
filmboy3 <jonathanschwartz30@gmail.com>
2018-12-01 18:02:43 -0500

    fixed dist path in server

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static('dist'));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'build', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   });
 }
 
