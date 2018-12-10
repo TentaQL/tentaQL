3aa346ba7ad2174686f8eba004324a8744872888
filmboy3 <jonathanschwartz30@gmail.com>
2018-12-10 05:25:55 -0500

    Added first nested mySQL unit test

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -7,11 +7,11 @@ const request = require('supertest')(url);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 
-describe('GraphQL MySQL Server Testing:', () => {
+describe('GraphQL Server Testing on MySQL:', () => {
   // Tests
-  it('Returns an instance of Dog Type dog properties, even if null', (done) => {
+  it('Query By ID returns a Dog with all dog properties, even if null', (done) => {
     request.post('graphql')
-    .send({ query: "query { Dog(id: 2) { name Owner species collarID id hasFleas favoriteFood } }"})
+    .send({ query: "query { Dog(id: 3) { name Owner species collarID id hasFleas favoriteFood } }"})
     .expect(200)
     .end((err, res) => {
       if (err) return done(err);
@@ -25,9 +25,9 @@ describe('GraphQL MySQL Server Testing:', () => {
     })
   })
 
-  it('Returns a dog that does not have any human properties ', (done) => {
+  it('Query By ID returns a Dog does not have any human properties ', (done) => {
     request.post('graphql')
-    .send({ query: "query { Dog(id: 2) { name Owner species collarID id hasFleas favoriteFood } }"})
+    .send({ query: "query { Dog(id: 3) { name Owner species collarID id hasFleas favoriteFood } }"})
     .expect(200)
     .end((err, res) => {
       if (err) return done(err);
@@ -37,7 +37,7 @@ describe('GraphQL MySQL Server Testing:', () => {
     })
   })
 
-  it('Return all Dogs', (done) => {
+  it('Query everyDogs returns all Dogs', (done) => {
     request.post('graphql')
     .send({ query: "query { everyDog { name Owner species collarID id hasFleas favoriteFood }}"})
     .expect(200)
@@ -50,5 +50,37 @@ describe('GraphQL MySQL Server Testing:', () => {
       done();
     })
   })
+
+  it('Returns Foreign Key Reference without throwing Error', (done) => {
+    request.post('graphql')
+    .send({ query: `query {
+      Dog(id:2) {
+        name
+        linkedHuman {
+          id
+          name
+          ageHumanYears
+          linkedDog {
+            id
+            name
+          }
+        }
+      }
+    }`})
+    .expect(200)
+    .end((err, res) => {
+      // res will contain array of all users
+      if (err) return done(err);
+      console.log("Result")
+      let Dog = res.body.data.Dog;
+      let Humans = Dog.linkedHuman;
+
+      // assume there are 12 users in the db
+      Humans[0].should.have.property("name");
+      done();
+    })
+  })
   
+  
+
 });
